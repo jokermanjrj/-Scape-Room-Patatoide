5844cc4caedcb3deb3ef63631817dbdaff2a9a6d
ruben <jokermanjrj@gmail.com>
2019-11-22 19:15:28 +0100

    Nuevo

---
+++ Scape Room/Assets/scripts/Cambiar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cambiar : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject panel1;
+
+    // Update is called once per frame
+    public void Cambiar_panel()
+    {
+        panel.SetActive(false);
+        panel1.SetActive(true);
+    }
+}
---
+++ Scape Room/Assets/scripts/Celda.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Celda 
+{
+    private bool salida;
+    private int daño;
+    public Celda(int daño)
+    {
+        this.daño = daño;
+        this.salida = false;
+    }
+
+    public bool esSalida()
+    {
+        return this.salida;
+    }
+
+    public void setSalida()
+    {
+        this.salida = true;
+    }
+
+    public int getdaño()
+    {
+        return this.daño;
+    }
+
+
+}
